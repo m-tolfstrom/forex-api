fb655855ff305069c1a0dda76cb15fe358db722d
12ström <jensen.m.tolfstrom@gmail.com>
2022-08-22 10:07:34 +0200

    BareBones
    Initial Commit

---
+++ src/test/java/se/jensensthlm/forexapi/ForexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensensthlm.forexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
